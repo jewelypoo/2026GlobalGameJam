4e422655472c61915cf0974d7e3e10a947b3bc48
Jewel <jewelxjocelyn@gmail.com>
2026-01-30 23:55:54 -0800

    set up button functionality

---
+++ game jam 2026/Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
