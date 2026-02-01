aa1738795db5b00741c798bed41dea9bd5df0b8f
lamomano <kanyonsr@gmail.com>
2026-02-01 00:01:25 -0800

    door stuck

--- game jam 2026/Assets/_Scripts/Anim/DoorOpen.cs
+++ game jam 2026/Assets/_Scripts/Anim/DoorOpen.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using UnityEngine;
 
-public class DoorOpen : MonoBehaviour
+public class DoorOpen : MonoBehaviour, IInteractable
 {
+    private bool usable = true;
+    [SerializeField] private AudioSource doorSound;
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            StartCoroutine(waitForOpen());
-        }
-    }
-
-   public IEnumerator waitForOpen()
+    private IEnumerator WaitForOpen()
     {
         GetComponent<Animator>().enabled = true;
+        doorSound.Play();
         yield return new WaitForSeconds(1);
         gameObject.GetComponent<BoxCollider>().enabled = false;
+
+    }
+
+
+    public void OnInteract()
+    {
+        if (!usable) return;
+        usable = false;
+        StartCoroutine(WaitForOpen());
+    }
+
+    public bool IsUsable() => usable;
+    public bool IsBeingUsed()
+    {
+        return false;
     }
 }
