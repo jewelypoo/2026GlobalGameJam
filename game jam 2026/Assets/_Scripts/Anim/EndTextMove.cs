cc37a2597e59f49c57cdd878786445a8782ada54
lamomano <kanyonsr@gmail.com>
2026-02-01 14:19:06 -0800

    finish menu has a quit button now yay

--- game jam 2026/Assets/_Scripts/Anim/EndTextMove.cs
+++ game jam 2026/Assets/_Scripts/Anim/EndTextMove.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework.Constraints;
+using System.Collections;
 using UnityEngine;
 
 public class EndTextMove : MonoBehaviour
@@ -5,10 +7,24 @@ public class EndTextMove : MonoBehaviour
     public float moveSpeed;
     private float distance;
 
+    [SerializeField] private GameObject quitCanvas;
+
+    private void Start()
+    {
+        quitCanvas.SetActive(false);
+        StartCoroutine(DelayShow());
+    }
+
     // Update is called once per frame
     void Update()
     {
         distance += moveSpeed * Time.deltaTime * .1f;
         transform.position = transform.position + new Vector3(0, distance, 0);
+    }
+
+    private IEnumerator DelayShow()
+    {
+        yield return new WaitForSeconds(20f);
+        quitCanvas.SetActive(true);
     }
 }
